a87aea88dc4fd43c2dd9d4c147e041e6e7e7f48f
Manoj1932 <manojmaruboina1431@gmail.com>
2025-12-02 12:19:32 +0530

    Refactor prediction handling and add predict page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import joblib
 import numpy as np
+import os
 
 app = FastAPI()
 
@@ -15,18 +16,28 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home():
-    with open("static/index.html", "r") as f:
-        return f.read()
+    with open("static/index.html") as f:
+        return HTMLResponse(f.read())
+
+
+@app.get("/predict", response_class=HTMLResponse)
+async def predict_page():
+    with open("static/predict.html") as f:
+        return HTMLResponse(f.read())
 
 
 @app.post("/predict")
 async def predict(request: Request):
-    data = await request.json()
+    data = await request.form()
+    try:
+        f1 = float(data["feature1"])
+        f2 = float(data["feature2"])
+        f3 = float(data["feature3"])
+        f4 = float(data["feature4"])
+    except:
+        return {"error": "Invalid input"}
 
-    feature1 = float(data["feature1"])
-    feature2 = float(data["feature2"])
+    x = np.array([[f1, f2, f3, f4]])
+    y_pred = model.predict(x)[0]
 
-    arr = np.array([[feature1, feature2]])
-    pred = int(model.predict(arr)[0])
-
-    return {"prediction": pred}
+    return {"prediction": int(y_pred)}
